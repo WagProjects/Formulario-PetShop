e002392c1fdf3210c18ea61375db8ca5ff3312e8
Wagner Oliveira <contato-wagneros@outlook.com>
2023-06-02 18:05:13 -0300

    Final

--- js/app.js
+++ js/app.js
@@ -3,6 +3,17 @@ import {valida} from "./validação/js"
 const inputs = documento.queryselectorAll('input')
 
 inputs.forEach(input => {
+    if(input.dataset.tipo === 'preco'){
+        SimpleMaskMoney.setMask(input, {
+            prefix: 'R$ ',
+            fixed: true,
+            fractionDigits: 2,
+            decimalSeparator: ',',
+            thousandsSeparator: '.',
+            cursor: 'end'
+        })
+    }
+
     input.addEventListener('blur', (evento) => {
         valida(evento.target)
     })
--- js/validacao.js
+++ js/validacao.js
@@ -54,6 +54,9 @@ const mensagensDeErro = {
     },
     estado:{
         valueMissing: 'O campo de Estado não pode estar vazio.',
+    },
+    preco:{
+        valueMissing: 'O campo de preço não pode estar vazio'
     }
 }
 
